7a8d41d3da44ded804ecd931c39382cc91974841
babamyrat <-l>
2021-07-05 19:43:30 +0300

    Revert "Start 04"
    This reverts commit ff162d75

--- app/src/main/java/com/example/json_url_recyclerview_4/ProfileActivity.java
+++ app/src/main/java/com/example/json_url_recyclerview_4/ProfileActivity.java
@@ -2,20 +2,11 @@ package com.example.json_url_recyclerview_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-
 public class ProfileActivity extends AppCompatActivity {
-    TextView textView, textView2, textView3;
-    ImageView imageView;
-    String name, species, gender,img;
-    Intent intent;
-    ExampleItem exampleItem;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,40 +16,16 @@ public class ProfileActivity extends AppCompatActivity {
         // back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        textView = findViewById(R.id.textView);
 
-        intent = getIntent();
-        exampleItem = intent.getParcelableExtra("Example Item");
+        String username = "Username not set";
 
-        name = exampleItem.getName();
-        species = exampleItem.getSpecies();
-        gender = exampleItem.getGender();
-        img = exampleItem.getImg();
-
-
-        imageView = findViewById(R.id.imageView2);
-
-
-        try {
-            // using Glide
-            Glide.with(this)
-                    .load(img)
-                    .into(imageView);
-        } catch (Exception ignored) {
-
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            username = extras.getString("name");
         }
 
-
-
-        textView = findViewById(R.id.textView);
-        textView.setText(name);
-
-        textView2 = findViewById(R.id.textView2);
-        textView2.setText(species);
-
-        textView3 = findViewById(R.id.textView3);
-        textView3.setText(gender);
-
-
+        textView.setText(username);
 
     }
 }
